22f02157ae0bdf6af900aca7c01973ccf6e1341a
Alexander Kulyk <akulyk@yandex.ua>
2021-05-17 00:39:47 +0300

    task "MeetupTitle" done

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,44 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// Требуется создать Vue приложение
+
+const app = new Vue({
+
+  data(){
+
+    return {
+      meetupsAmount: 5,
+      meetups: [],
+    };
+
+  },
+
+  created(){
+    for(let i = 1; i <= this.meetupsAmount; i++){
+      this.meetups.push({
+        id:i,
+        title: null,
+        checked: false,
+      })
+    }
+  },
+
+  methods:{
+    async handleClick(e){
+      const id = +e.target.value;
+      const meetup = this.meetups
+        .map((meetup) => {
+          meetup.checked = false;
+          return meetup;
+        })
+        .find((m) => m.id === id);
+      if (!meetup) {
+        return;
+      }
+      const response = await fetch(`https://course-vue.javascript.ru/api/meetups/${id}`).then((res) => res.json());
+      meetup.checked = true;
+      meetup.title = response.title;
+    }
+  }
+});
+
+app.$mount('#app');
