3f69d041bf2b433171f34b5453f30c9c6f8689e6
Alexander Kulyk <akulyk@yandex.ua>
2021-05-16 23:51:43 +0300

    task "MarkedList" done

--- 01-basics/02-MarkedList/script.js
+++ 01-basics/02-MarkedList/script.js
@@ -1,7 +1,8 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
+import {debounce} from "./vendor/utils.js";
 
 // From https://jsonplaceholder.typicode.com/comments
-/*
+
 const emails = [
   'Eliseo@gardner.biz',
   'Jayne_Kuhic@sydney.com',
@@ -29,6 +30,43 @@ const emails = [
   'Jeffery@juwan.us',
   'Isaias_Kuhic@jarrett.net',
 ];
-*/
 
 // Требуется создать Vue приложение
+const app = new Vue({
+  data() {
+    return {
+      emails: [],
+      term: null,
+    };
+  },
+
+  computed: {
+    found() {
+      const term = this.term;
+      let found = term ? this.emails.filter((email) => email.toLowerCase().search(term) !== -1) : [];
+      if (!found) {
+        found = [];
+      }
+
+      return found;
+    },
+  },
+
+  created() {
+    this.emails = emails;
+  },
+
+  methods: {
+    debouncedSearch(e, delay) {
+      delay = delay || 1000;
+      const search = debounce(this.handleInput, delay);
+      return search(e);
+    },
+
+    handleInput(e) {
+      this.term = e ? e.target.value.toLowerCase() : null;
+    },
+  },
+});
+
+app.$mount('#app');
