f5259af47cd583d58f6614e6256f76b4cfa812ce
Alexander Kulyk <akulyk@yandex.ua>
2021-05-22 14:52:24 +0300

    Task 03/03-TheToaster done

---
+++ 03-sfc/03-TheToaster/components/ToasterMessage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="toast" :class="classType">
+    <app-icon :icon="icon"/>
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<script>
+import AppIcon from './AppIcon';
+
+const typeMap = {
+  success: {
+    class: 'toast_success',
+    icon: 'check-circle',
+  },
+  error: {
+    class: 'toast_error',
+    icon: 'alert-circle',
+  },
+};
+export default {
+  name: 'ToasterMessage',
+  components: {AppIcon},
+  props: {
+    type: {
+      type: String,
+      required: true,
+      validate: (name) => Object.keys(typeMap).includes(name),
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    classType() {
+      return typeMap[this.type].class;
+    },
+    icon() {
+      return typeMap[this.type].icon;
+    }
+  },
+};
+</script>
+
+<style scoped></style>
