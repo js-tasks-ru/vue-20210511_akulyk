0c794522dd58896b77c2d50dd18b7df28e0f30b4
Alexander Kulyk <akulyk@yandex.ua>
2021-06-03 00:07:54 +0300

    Task 04/06-QuerySync done

--- 04-spa/06-QuerySync/views/QuerySync.vue
+++ 04-spa/06-QuerySync/views/QuerySync.vue
@@ -1,13 +1,55 @@
 <template>
-  <meetups-view />
+  <meetups-view
+    v-bind="currentRoute.query"
+    @update:view="updateProp('view',$event)"
+    @update:date="updateProp('date',$event)"
+    @update:participation="updateProp('participation',$event)"
+    @update:search="updateProp('search',$event)"
+  />
 </template>
 
 <script>
 import MeetupsView from '../components/MeetupsView';
+import VueRouter from 'vue-router';
+import {defaults} from '../utils.js';
+
+const {isNavigationFailure, NavigationFailureType} = VueRouter;
+
 
 export default {
   name: 'QuerySync',
+  components: {MeetupsView},
+  props: Object.keys(defaults),
+  computed: {
+    currentRoute() {
+      const query = {...this.$route.query};
+      return {path: '/', query};
+    },
+  },
+  methods: {
+    updateProp(prop, value) {
+      const route = {...this.currentRoute};
+      route.query = {...route.query, [prop]: value};
+      this.changeRoute(route);
+    },
 
-  components: { MeetupsView },
+    changeRoute(route) {
+      route.query = this.removeDefaults(route.query);
+      this.$router.push(route).catch((failure) => {
+        if (!isNavigationFailure(failure, NavigationFailureType.duplicated)) {
+          throw failure;
+        }
+      });
+    },
+
+    removeDefaults(query) {
+      for (const name in query) {
+        if (query[name] === defaults[name]) {
+          delete query[name];
+        }
+      }
+      return {...query};
+    },
+  },
 };
 </script>
