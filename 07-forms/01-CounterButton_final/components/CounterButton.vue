75ee77a8b60cfad77cfe3c55065d703b2d1ca74d
Alexander Kulyk <akulyk@yandex.ua>
2021-06-21 23:17:33 +0300

    Task 07/01-CounterButton_final done

--- 07-forms/01-CounterButton_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_final/components/CounterButton.vue
@@ -1,9 +1,42 @@
 <template>
-  <button></button>
+  <button class="btn btn-primary btn-sm" @click="handleClick">{{increment}}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  data: () => ({
+    increment: 0,
+  }),
+  
+  created() {
+    this.increment = this.count;
+  },
+
+  watch: {
+    count: function (newValue) {
+        this.increment = newValue;
+        this.$emit('increment',newValue);
+    },
+  },
+
+  methods: {
+    handleClick() {
+      this.increment++;
+      this.$emit('increment',this.increment);
+    },
+  },
 };
 </script>
