685737e677f676587f82335f859841abdf291928
Alexander Kulyk <akulyk@yandex.ua>
2021-08-18 23:10:25 +0300

    Task 08-deep-vue/02-NodesPagination done

--- 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
+++ 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
@@ -1,5 +1,29 @@
 <script>
 export default {
   name: 'NodesPagination',
+  props: {
+    page: {
+      type: Number,
+    },
+
+    perPage: {
+      type: Number,
+    },
+  },
+
+  render(createElement) {
+    const page = this.page || 1;
+    const start = (page - 1) * this.perPage;
+    const end = this.perPage ? start + this.perPage : undefined;
+
+    let children = [];
+    if (this.$slots.default && Array.isArray(this.$slots.default)){
+      children = this.$slots.default.slice(start, end);
+    }
+    return createElement(
+      'div', // имя тега
+      children, // массив дочерних элементов
+    );
+  },
 };
 </script>
